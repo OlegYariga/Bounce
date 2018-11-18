954908d126f9b6c79284839ff6cc5598f94465fb
YarigaOleg <Oyariga@gmail.com>
2018-11-18 14:34:27 +0300

    Add files Boost.h and Boost.cpp

---
+++ Bounce/Boost.cpp
@@ -0,0 +1,9 @@
+#pragma once
+#include "Boost.h"
+
+Boost::Boost() {
+	buff.loadFromFile("food-icons-floating.png");
+	sprite.setTexture(buff);
+	sprite.setTextureRect(IntRect(0, 20, 15, 10));
+	sprite.setPosition(60, 350);
+}
---
+++ Bounce/Boost.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+class Boost {
+public:
+	sf::Sprite sprite;
+	Boost();
+private:
+	Texture buff;
+};
