3cdf828457ce0eb2001ce7c2ee480dcfdb224771
FedoseevIlya <Ilafedoseev@mail.ru>
2018-11-19 16:29:48 +0300

    Nothing added. Changes in Boost.cpp

--- Bounce/Boost.cpp
+++ Bounce/Boost.cpp
@@ -4,6 +4,6 @@
 Boost::Boost() {
 	buff.loadFromFile("food-icons-floating.png");
 	sprite.setTexture(buff);
-	sprite.setTextureRect(IntRect(0, 20, 15, 10));
-	sprite.setPosition(50, 50);
+	sprite.setTextureRect(IntRect(191, 35 , 32, 32));
+	sprite.setPosition(50, 70);
 }
